5e3737ef506bc4b475da46658348156db413e9d5
yanjianliao <yanjianliao@ryous-MacBook-Pro.local>
2018-05-15 16:55:26 -0700

    Created login css and login js files

---
+++ src/main/webapp/jquery/login/login.controller.client.js
@@ -0,0 +1,18 @@
+(function () {
+    var $usernameFld, $passwordFld;
+    var $loginBtn;
+    var userService = new UserServiceClient();
+    $(main);
+
+    function main() {
+        $usernameFld = $('#username');
+        $passwordFld = $('#password');
+        $loginBtn = $('#loginBtn');
+
+    }
+
+    function login() {
+        
+    }
+
+})();
